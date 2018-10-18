7e5eca2c20e59e483c8b7248e54faf3671b74072
karina-wilk <car87@poczta.fm>
2018-10-19 01:04:16 +0200

    ZMiany w branchu

--- src/com/sdacademy/Main.java
+++ src/com/sdacademy/Main.java
@@ -5,13 +5,16 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// write your code here\
+        Scanner input = new Scanner(System.in);
+        System.out.print("Podaj znak");
+        char zn = input.next().charAt(0);
         System.out.print("To jest dodawacz. Podaj 2 liczby rozdzielajc je ENTER");
 
-        Scanner input = new Scanner(System.in);
+
         int a = input.nextInt();
         int b = input.nextInt();
 
-        System.out.print("WYnik: "+ (a + b));
+        System.out.print("WYnik: "+ ((zn == '+')? (a + b) : (a-b)));
     }
 }
